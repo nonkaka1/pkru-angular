4a4a2c7d5cfd5d72706298bcfbb1bae729212792
RoseSkeleton <pooton55@hotmail.com>
2018-07-23 16:09:25 +0700

    component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { ContactComponent } from './contact/contact.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { JumpbottonComponent } from './jumpbotton/jumpbotton.component';
+import { BlogComponent } from './blog/blog.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactComponent,
+    NavbarComponent,
+    JumpbottonComponent,
+    BlogComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,FormsModule
