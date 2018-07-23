56fca2e6de2fb07d0dddb3850406b0e963f4e0d9
RoseSkeleton <pooton55@hotmail.com>
2018-07-23 13:33:01 +0700

    init project

---
+++ src/app/app.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title:string = 'BoBO';
+  version:Number = 1.0;
+  disabled:Boolean = false;
+
+  changeTitle(event){
+    console.log(event.target.value);
+    this.title = event.target.value;
+  }
+
+  changeVersion(event){
+    console.log(event.target.value);
+    this.version = event.target.value;
+  }
+
+}
